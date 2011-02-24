e1cf845a55b43d43fac73b236e1fcafaaaf4a48a
Jeremy Cutler <cutler.jeremy@gmail.com>
2011-02-24 09:21:23 -0500

    Fix to script load request Only execute scripts on inner frames for inlined iframes.

--- chrome/whoami.js
+++ chrome/whoami.js
@@ -1,10 +1,13 @@
-chrome.extension.sendRequest({greeting: "start", url: window.location.href}, function(response){
-   var active = JSON.parse(response);
-   var info = "'Missing e' Startup on ";
-   info += active.url + "\n";
-   for (var i in active) {
-      if (i != 'url')
-         info += i + ": active\n";
-   }
-   console.log(info);
-});
+if (window.top == window ||
+    /http:\/\/www\.tumblr\.com\/dashboard\/iframe/.test(window.location.href)) {
+   chrome.extension.sendRequest({greeting: "start", url: window.location.href}, function(response){
+      var active = JSON.parse(response);
+      var info = "'Missing e' Startup on ";
+      info += active.url + "\n";
+      for (var i in active) {
+         if (i != 'url')
+            info += i + ": active\n";
+      }
+      console.log(info);
+   });
+}
