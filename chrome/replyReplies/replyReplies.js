298ebeac53f89d87e53b7192a43778053a2995ac
Jeremy Cutler <cutler.jeremy@gmail.com>
2011-02-25 15:21:56 -0500

    Add copyright, license, inclusion info Also update about content

--- chrome/replyReplies/replyReplies.js
+++ chrome/replyReplies/replyReplies.js
@@ -1,3 +1,26 @@
+/*
+ * 'Missing e' Extension
+ *
+ * Copyright 2011, Jeremy Cutler
+ * Licensed under the GPL Version 2 licence.
+ * SEE: GPL-LICENSE.txt
+ *
+ * This file is part of 'Missing e'.
+ *
+ * 'Missing e' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 'Missing e' is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 'Missing e'.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 function reply_getValue() {
    var retval = window.localStorage.getItem("trr_ReplyText");
    if (retval == undefined || retval == null || retval == "")
