c4f7f0d664a64291083cf17473d46f4fea72be45
Jeremy Cutler <cutler.jeremy@gmail.com>
2011-02-25 16:37:42 -0500

    Fix build script

--- build/buildChrome.bat
+++ build/buildChrome.bat
@@ -2,7 +2,7 @@
 
 cp -R ../chrome .
 rename chrome MissingE
-mkdir chrome
-rm chrome/missinge.zip
-"C:\Program Files\7-Zip\7z.exe" a chrome\missinge.zip MissingE
+mkdir chrome-build
+rm chrome-build/missinge.zip
+"C:\Program Files\7-Zip\7z.exe" a chrome-build\missinge.zip MissingE
 rm -rf MissingE
