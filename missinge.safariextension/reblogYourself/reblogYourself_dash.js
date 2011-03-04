df06a79e425bb69d27edd9b392e7c474a2d47b22
Jeremy Cutler <cutler.jeremy@gmail.com>
2011-03-04 16:00:04 -0500

    Add dashboard reblogging for secondary accts

--- missinge.safariextension/reblogYourself/reblogYourself_dash.js
+++ missinge.safariextension/reblogYourself/reblogYourself_dash.js
@@ -22,8 +22,9 @@
  */
 
 function addReblog(item) {
-   if (item.tagName == "LI" && $(item).hasClass('post') && $(item).hasClass('is_mine') && !$(item).hasClass('new_post')) {
-      if ($(item).find('div.post_controls a:contains("reblog")').length > 0)
+   if (item.tagName == "LI" && $(item).hasClass('post') && !$(item).hasClass('new_post') && !$(item).hasClass('note')) {
+      if ($(item).find('div.post_controls a:contains("reblog")').length > 0 ||
+          $(item).find('div.post_controls a:contains("edit")').length == 0)
          return true;
       var tid = $(item).attr("id").match(/[0-9]*$/)[0];
       var addr = $(item).find("a.permalink:first").attr("href").match(/http:\/\/[^\/]*/)[0];
@@ -46,7 +47,7 @@ function MissingE_reblogYourself_dash_doStartup() {
    if (/drafts$/.test(location) == false &&
        /queue$/.test(location) == false &&
        /messages$/.test(location) == false) {
-      $('#posts li.is_mine').each(function(){addReblog(this);});
+      $('#posts li.post').each(function(){addReblog(this);});
       document.addEventListener('DOMNodeInserted',function(e) {
          addReblog(e.target);
       }, false);
