1fc5bc67433bba5986a5c94bdde586e472a558cc
Jeremy Cutler <cutler.jeremy@gmail.com>
2011-03-21 15:28:13 -0400

    Revert "Adding first-shot attempt at fixing infinite scroll failures"
    This reverts commit a356854151c5c94afb618f68d9a88acc41bed5ae.

--- chrome/common/whoami.js
+++ chrome/common/whoami.js
@@ -41,20 +41,4 @@ if ((window.top === window &&
       }
       console.log(info);
    });
-
-   /*
-   var scr = document.createElement('script');
-   scr.setAttribute('type','text/javascript');
-   scr.innerHTML = 'Ajax.Responders.register({' +
-      'onComplete: function(response){' +
-         'if ((/\\/dashboard\\/[0-9]+\\/[0-9]+\\?lite$/.test(response.url)) && ' +
-              'response.transport.status === 200 && ' +
-              '!(/<!-- START POSTS -->/.test(response.transport.responseText))) {' +
-            'console.log("SAVED BY ME!");' +
-            'response.options.onFailure();' +
-         '}' +
-      '}' +
-   '});';
-   document.getElementsByTagName('head')[0].appendChild(scr);
-   */
 }
