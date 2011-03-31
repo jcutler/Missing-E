de198105f1033c2e426322dc6785cb2fbb2f0bf6
Jeremy Cutler <cutler.jeremy@gmail.com>
2011-03-31 09:25:48 -0400

    add build scripts for ff and all

---
+++ build/buildFirefox.sh
@@ -0,0 +1,19 @@
+CWD=$PWD
+BUILDDIR=$PWD/firefox-build
+JETPACK=`find /d -maxdepth 1 -name "addon-sdk*" -type d`
+FF="d:/Missing-E/firefox/missinge"
+rm -rf $BUILDDIR/*
+
+cd $JETPACK
+
+source bin/activate
+
+cd $FF
+
+cfx xpi
+
+mv missinge.xpi $BUILDDIR
+
+"c:/Program Files/Mozilla Firefox/firefox.exe" "https://addons.mozilla.org/en-US/developers/addon/missing-e/versions/#version-upload"
+
+cd $CWD
