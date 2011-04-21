81a4829a858ab760c9f3d4606425b7a9388c0573
Jeremy Cutler <cutler.jeremy@gmail.com>
2011-04-21 14:08:25 -0400

    Add better reblogs (pass tags only) to Chrome

---
+++ chrome/betterReblogs/betterReblogs_dash.js
@@ -0,0 +1,59 @@
+/*
+ * 'Missing e' Extension
+ *
+ * Copyright 2011, Jeremy Cutler
+ * Released under the GPL version 3 licence.
+ * SEE: GPL-LICENSE.txt
+ *
+ * This file is part of 'Missing e'.
+ *
+ * 'Missing e' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 'Missing e' is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 'Missing e'. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function setReblogTags(tags) {
+   console.log(tags.join(','));
+   localStorage.setItem('tbr_ReblogTags',tags.join(','));
+}
+
+chrome.extension.sendRequest({greeting: "settings", component: "betterReblogs"},
+                             function(response) {
+   var reblog_settings = JSON.parse(response);
+
+   if (reblog_settings.passTags === 1) {
+      $('#posts div.post_controls a[title="reblog"]').live('mouseup', function(e) {
+         if (e.which !== 1 && e.which !== 2) { return; }
+         var tags = $(this).closest('li.post').find('span.tags a');
+         var tagarr = [];
+         if (/http:\/\/www\.tumblr\.com\/tagged\//.test(location.href)) {
+            var i;
+            var str = location.href.match(/[^\/\?]*(?:$|\?)/)[0];
+            str = str.replace(/\?/,'').replace(/\+/,' ');
+            var entities = str.match(/%[0-9a-fA-F]{2}/g);
+            if (entities !== undefined && entities !== null) {
+               for (i=0; i<entities.length; i++) {
+                  var repl = String.fromCharCode(parseInt(entities[i].replace(/%/,''),16));
+                  console.log(entities[i] + " = " + repl);
+                  str = str.replace(entities[i],repl);
+               }
+            }
+            tagarr.push(str);
+         }
+         tags.each(function() {
+            tagarr.push($(this).html().replace(/^#/,''));
+         });
+         setReblogTags(tagarr);
+      });
+   }
+});
+
---
+++ chrome/betterReblogs/betterReblogs_post.js
@@ -0,0 +1,77 @@
+/*
+ * 'Missing e' Extension
+ *
+ * Copyright 2011, Jeremy Cutler
+ * Released under the GPL version 3 licence.
+ * SEE: GPL-LICENSE.txt
+ *
+ * This file is part of 'Missing e'.
+ *
+ * 'Missing e' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 'Missing e' is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 'Missing e'. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function setReblogTags(tags) {
+   localStorage.setItem('tbr_ReblogTags',tags.join(','));
+}
+
+function addTags(link) {
+   var i;
+   if (link === undefined || link === null) {
+      var div = document.getElementsByTagName("div")[0];
+      var controls = div.getElementsByTagName("a");
+      for (i=0; i<controls.length; i++) {
+         if (/reblog/.test(controls[i].href)) {
+            link = controls[i];
+            break;
+         }
+      }
+      if (link === undefined || link === null) {
+         return false;
+      }
+   }
+
+   var host, pid;
+   var loc = location.href;
+   loc = loc.substring(loc.indexOf("src=")+4);
+   loc = loc.replace(/%3A/gi,":")
+            .replace(/%2F/gi,"/");
+   host = loc.match(/http:\/\/[^\/]*/)[0];
+   pid = loc.match(/&pid=([0-9]*)/)[1];
+   chrome.extension.sendRequest({greeting: "tags", pid: pid, url: host},
+                                function(response) {
+      if (response.success) {
+         link.setAttribute('tags',response.data.join(','));
+      }
+   });
+
+   link.addEventListener('mouseup',function(e){
+      if (e.which === 1 || e.which === 2) {
+         setReblogTags(this.getAttribute('tags').split(','));
+      }
+   }, false);
+
+   return true;
+}
+
+if (/http:\/\/www\.tumblr\.com\/dashboard\/iframe/.test(location.href)) {
+   if (!addTags()) {
+      document.addEventListener('DOMNodeInserted',function(e){
+         var item = e.target;
+         if (item.tagName === 'A' && /reblog/.test(item.href)) {
+            addTags();
+         }
+      }, false);
+   }
+}
+
