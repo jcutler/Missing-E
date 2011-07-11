0651b4b996f7e6413f96943fbe6f2f1635d426ed
Jeremy Cutler <cutler.jeremy@gmail.com>
2011-07-11 14:22:09 -0400

    change nav bar links to correct href

--- site/update/index.php
+++ site/update/index.php
@@ -28,7 +28,7 @@ jQuery(document).ready(function($) {
 </head>
 <body id="faq">
 <div id="logo"><a name="top"></a><a href="/"><img src="/images/missinge.png" alt="Missing e" /></a></div>
-<div id="map"><a href="/">GET</a><a href="about">ABOUT</a><a href="features">FEATURES</a><a href="faq">FAQ</a><a href="http://blog.missinge.infraware.ca">BLOG</a></div><div id="content">
+<div id="map"><a href="/">GET</a><a href="/about">ABOUT</a><a href="/features">FEATURES</a><a href="/faq">FAQ</a><a href="http://blog.missinge.infraware.ca">BLOG</a></div><div id="content">
 <div class="clear"></div>
 <h1><em>Missing e</em> has been updated</h1>
 <h2 style="text-align:center;background-color:transparent;"><a href="<?=urldecode($_GET["l"]);?>">Read about it here</a></h2>
