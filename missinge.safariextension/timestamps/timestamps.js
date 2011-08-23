cc709b598b51a7edf67ebd5c84cbfe4ef0b7fad0
Jeremy Cutler <cutler.jeremy@gmail.com>
2011-08-23 14:32:39 -0400

    Timestamps back in Safari (currently not run for dash)

--- missinge.safariextension/timestamps/timestamps.js
+++ missinge.safariextension/timestamps/timestamps.js
@@ -25,8 +25,35 @@
 
 function loadTimestamp(item) {
    var lang = $('html').attr('lang');
-   if (item.tagName === "LI" && $(item).hasClass("post")
-       && $(item).attr("id") !== "new_post") {
+   /* Comment this section to enable timestamps */
+   if (!/^\/(inbox|messages|submissions)/.test(location.pathname) &&
+       !/^\/tumblelog\/[^\/]*\/(messages|submissions)/
+         .test(location.pathname)) {
+      return false;
+   }
+   /* */
+   if (item.tagName === "LI" && $(item).hasClass("post") &&
+       $(item).attr("id") !== "new_post" &&
+       $(item).find('.private_label').length === 0) {
+      var tid = $(item).attr("id").match(/[0-9]*$/)[0];
+      var perm = $(item).find("a.permalink:first");
+      var addr, type, stamp;
+      if (/^\/(inbox|messages|submissions)/.test(location.pathname) ||
+          /^\/tumblelog\/[^\/]*\/(messages|submissions)/
+            .test(location.pathname)) {
+         type = 'ask';
+         addr = 'http://www.tumblr.com/edit/';
+         stamp = '';
+      }
+      else if (perm.length > 0) {
+         type = 'other';
+         addr = '';
+         stamp = perm.attr('title').replace(/^.* \- /,'');
+      }
+      if (tid === undefined || tid === null || tid === "" ||
+          addr === undefined || stamp === undefined || stamp === null) {
+         return;
+      }
       var div = $(item).find("div.post_info");
       if (div.length === 0) {
          $(item).find(".post_controls:first")
@@ -48,18 +75,9 @@ function loadTimestamp(item) {
             spn.text(locale[lang].loading);
          }
       }
-      var tid = $(item).attr("id").match(/[0-9]*$/)[0];
-      var perm = $(item).find("a.permalink:first");
-      var addr;
-      if (/^\/(inbox|messages|submissions)/.test(location.pathname) ||
-          /^\/tumblelog\/[^\/]*\/(messages|submissions)/
-            .test(location.pathname)) {
-         addr = 'http://www.tumblr.com/edit/';
-      }
-      if (tid === undefined || tid === null || tid === "" ||
-          addr === undefined) { return; }
       safari.self.tab.dispatchMessage("timestamp", {pid: tid, url: addr,
-                                                    lang: lang});
+                                                    lang: lang, stamp: stamp,
+                                                    type: type});
    }
 }
 
