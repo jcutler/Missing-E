55360142c697fcb52c3fd6732cf1f5cbff226e31
Jeremy Cutler <cutler.jeremy@gmail.com>
2011-11-24 13:28:53 -0500

    extendable set environment script

--- setenvironment.sh
+++ setenvironment.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
 CWD=`dirname $0`
+COMMON="core license"
 
-if [[ ! -e "$CWD/chrome/core" ]]; then
-   echo "Creating junction for Chrome: core"
-   junction $CWD/chrome/core $CWD/core
-fi
-if [[ ! -e "$CWD/chrome/license" ]]; then
-   echo "Creating junction for Chrome: license"
-   junction $CWD/chrome/license $CWD/license
-fi
-if [[ ! -e "$CWD/missinge.safariextension/core" ]]; then
-   echo "Creating junction for Safari: core"
-   junction $CWD/missinge.safariextension/core $CWD/core
-fi
-if [[ ! -e "$CWD/missinge.safariextension/license" ]]; then
-   echo "Creating junction for Safari: license"
-   junction $CWD/missinge.safariextension/license $CWD/license
-fi
-if [[ ! -e "$CWD/firefox/missinge/data/core" ]]; then
-   echo "Creating junction for Firefox: core"
-   junction $CWD/firefox/missinge/data/core $CWD/core
-fi
-if [[ ! -e "$CWD/firefox/missinge/data/license" ]]; then
-   echo "Creating junction for Firefox: license"
-   junction $CWD/firefox/missinge/data/license $CWD/license
-fi
+for i in $COMMON; do
+   if [[ ! -e "$CWD/chrome/$i" ]]; then
+      echo "Creating junction for Chrome: $i"
+      junction $CWD/chrome/$i $CWD/$i
+   fi
+   if [[ ! -e "$CWD/missinge.safariextension/$i" ]]; then
+      echo "Creating junction for Safari: $i"
+      junction $CWD/missinge.safariextension/$i $CWD/$i
+   fi
+   if [[ ! -e "$CWD/firefox/missinge/data/$i" ]]; then
+      echo "Creating junction for Firefox: $i"
+      junction $CWD/firefox/missinge/data/$i $CWD/$i
+   fi
+done
