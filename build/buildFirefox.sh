735ec8d7d87ec72d80c542aeb4570e61b443889a
Jeremy Cutler <cutler.jeremy@gmail.com>
2011-12-05 18:52:31 -0500

    Make build scripts compatible with OSX

--- build/buildFirefox.sh
+++ build/buildFirefox.sh
@@ -1,7 +1,9 @@
 CWD=$PWD
 BUILDDIR=$PWD/firefox-build
-JETPACK=`find /d -maxdepth 1 -name "addon-sdk*" -type d`
-FF="d:/Missing-E/firefox/missinge"
+JETPACK=`find ../../ -maxdepth 1 -name "addon-sdk*" -type d`
+FF=$PWD/../firefox/missinge
+ZIP=`which zip`
+UNZIP=`which unzip`
 SEVZ="c:/Program Files/7-Zip/7z.exe"
 rm -rf $BUILDDIR/*
 
@@ -19,7 +21,11 @@ mv missinge.xpi $BUILDDIR/missinge/missinge.zip
 
 cd $BUILDDIR/missinge
 
-"$SEVZ" x missinge.zip
+if [[ -n "$UNZIP" ]]; then
+   $UNZIP missinge.zip
+else
+   "$SEVZ" x missinge.zip
+fi
 
 rm missinge.zip
 mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data resources/data
@@ -30,51 +36,55 @@ mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib resources
 mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data resources/api-utils-data
 mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib resources/api-utils-lib
 
-sed -e '/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data":\s*\[/{
+sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data":[ ]*\[/{
 N; N;
-s/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data":\s*\[\s*\n\s*"resources",\s*\n\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data"/  "jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data": \[\n   "resources", \n   "data"/
+s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data"/\1"data"/
 }' harness-options.json > harness-options.new.0
 
-sed -e '/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib":\s*\[/{
+sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib":[ ]*\[/{
 N; N;
-s/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib":\s*\[\s*\n\s*"resources",\s*\n\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib"/  "jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib": \[\n   "resources", \n   "lib"/
+s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib"/\1"lib"/
 }' harness-options.new.0 > harness-options.new.1
 
-sed -e '/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests":\s*\[/{
+sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests":[ ]*\[/{
 N; N;
-s/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests":\s*\[\s*\n\s*"resources",\s*\n\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests"/  "jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests": \[\n   "resources", \n   "tests"/
+s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests"/\1"tests"/
 }' harness-options.new.1 > harness-options.new.2
 
-sed -e '/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data":\s*\[/{
+sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data":[ ]*\[/{
 N; N;
-s/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data":\s*\[\s*\n\s*"resources",\s*\n\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data"/  "jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data": \[\n   "resources", \n   "addon-kit-data"/
+s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data"/\1"addon-kit-data"/
 }' harness-options.new.2 > harness-options.new.3
 
-sed -e '/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib":\s*\[/{
+sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib":[ ]*\[/{
 N; N;
-s/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib":\s*\[\s*\n\s*"resources",\s*\n\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib"/  "jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib": \[\n   "resources", \n   "addon-kit-lib"/
+s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib"/\1"addon-kit-lib"/
 }' harness-options.new.3 > harness-options.new.4
 
-sed -e '/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data":\s*\[/{
+sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data":[ ]*\[/{
 N; N;
-s/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data":\s*\[\s*\n\s*"resources",\s*\n\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data"/  "jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data": \[\n   "resources", \n   "api-utils-data"/
+s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data"/\1"api-utils-data"/
 }' harness-options.new.4 > harness-options.new.5
 
-sed -e '/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib":\s*\[/{
+sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib":[ ]*\[/{
 N; N;
-s/\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib":\s*\[\s*\n\s*"resources",\s*\n\s*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib"/  "jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib": \[\n   "resources", \n   "api-utils-lib"/
+s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib"/\1"api-utils-lib"/
 }' harness-options.new.5 > harness-options.new.6
 
 mv harness-options.new.6 harness-options.json
 rm -f harness-options.new.*
 
-sed -e 's/<em:maxVersion>[^<]*/<em:maxVersion>11.0a1/' install.rdf > install.new.0
-sed -e 's/<em:minVersion>[^<]*/<em:minVersion>7.0/' install.new.0 > install.new.1
+sed -E -e 's/<em:maxVersion>[^<]*/<em:maxVersion>11.0a1/' install.rdf > install.new.0
+sed -E -e 's/<em:minVersion>[^<]*/<em:minVersion>7.0/' install.new.0 > install.new.1
 
 mv install.new.1 install.rdf
 rm -f install.new.*
 
-"$SEVZ" a missinge.zip *
+if [[ -n "$ZIP" ]]; then
+   $ZIP -r missinge.zip *
+else
+   "$SEVZ" a missinge.zip *
+fi
 
 mv missinge.zip $BUILDDIR/missinge.xpi
 
@@ -88,5 +98,10 @@ if [[ "$1" == "-noupload" ]]; then
    exit 0
 fi
 
-"c:/Program Files/Mozilla Firefox/firefox.exe" "https://addons.mozilla.org/en-US/developers/addon/missing-e/versions/#version-upload"
+SYS=`uname`
 
+if [[ "$SYS" == "Darwin" ]]; then
+   open -a Firefox "https://addons.mozilla.org/en-US/developers/addon/missing-e/versions/#version-upload"
+else
+   "c:/Program Files/Mozilla Firefox/firefox.exe" "https://addons.mozilla.org/en-US/developers/addon/missing-e/versions/#version-upload"
+fi
