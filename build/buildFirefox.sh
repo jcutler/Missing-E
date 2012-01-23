66ce6aca62a70f181ad200921b3ce6405fc18d2b
Jeremy Cutler <cutler.jeremy@gmail.com>
2012-01-22 22:15:51 -0500

    no longer mess with firefox internal stuff

--- build/buildFirefox.sh
+++ build/buildFirefox.sh
@@ -29,51 +29,51 @@ else
 fi
 
 rm missinge.zip
-mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data resources/data
-mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib resources/lib
-mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests resources/tests
-mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data resources/addon-kit-data
-mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib resources/addon-kit-lib
-mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data resources/api-utils-data
-mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib resources/api-utils-lib
+#mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data resources/data
+#mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib resources/lib
+#mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests resources/tests
+#mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data resources/addon-kit-data
+#mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib resources/addon-kit-lib
+#mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data resources/api-utils-data
+#mv resources/jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib resources/api-utils-lib
 
-sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data":[ ]*\[/{
-N; N;
-s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data"/\1"data"/
-}' harness-options.json > harness-options.new.0
+#sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data":[ ]*\[/{
+#N; N;
+#s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-data"/\1"data"/
+#}' harness-options.json > harness-options.new.0
 
-sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib":[ ]*\[/{
-N; N;
-s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib"/\1"lib"/
-}' harness-options.new.0 > harness-options.new.1
+#sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib":[ ]*\[/{
+#N; N;
+#s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-lib"/\1"lib"/
+#}' harness-options.new.0 > harness-options.new.1
 
-sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests":[ ]*\[/{
-N; N;
-s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests"/\1"tests"/
-}' harness-options.new.1 > harness-options.new.2
+#sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests":[ ]*\[/{
+#N; N;
+#s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-missinge-tests"/\1"tests"/
+#}' harness-options.new.1 > harness-options.new.2
 
-sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data":[ ]*\[/{
-N; N;
-s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data"/\1"addon-kit-data"/
-}' harness-options.new.2 > harness-options.new.3
+#sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data":[ ]*\[/{
+#N; N;
+#s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-data"/\1"addon-kit-data"/
+#}' harness-options.new.2 > harness-options.new.3
 
-sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib":[ ]*\[/{
-N; N;
-s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib"/\1"addon-kit-lib"/
-}' harness-options.new.3 > harness-options.new.4
+#sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib":[ ]*\[/{
+#N; N;
+#s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-addon-kit-lib"/\1"addon-kit-lib"/
+#}' harness-options.new.3 > harness-options.new.4
 
-sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data":[ ]*\[/{
-N; N;
-s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data"/\1"api-utils-data"/
-}' harness-options.new.4 > harness-options.new.5
+#sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data":[ ]*\[/{
+#N; N;
+#s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-data"/\1"api-utils-data"/
+#}' harness-options.new.4 > harness-options.new.5
 
-sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib":[ ]*\[/{
-N; N;
-s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib"/\1"api-utils-lib"/
-}' harness-options.new.5 > harness-options.new.6
+#sed -E -e '/[ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib":[ ]*\[/{
+#N; N;
+#s/([ ]*"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib":[ ]*\[[ ]*\n[ ]*"resources",[ ]*\n[ ]*)"jid0-0pgffacvvhubiefykrvvc5w6liu-at-jetpack-api-utils-lib"/\1"api-utils-lib"/
+#}' harness-options.new.5 > harness-options.new.6
 
-mv harness-options.new.6 harness-options.json
-rm -f harness-options.new.*
+#mv harness-options.new.6 harness-options.json
+#rm -f harness-options.new.*
 
 sed -E -e 's/<em:maxVersion>[^<]*/<em:maxVersion>12.0a1/' install.rdf > install.new.0
 sed -E -e 's/<em:minVersion>[^<]*/<em:minVersion>7.0/' install.new.0 > install.new.1
