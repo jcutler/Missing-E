1dcd1ba3b439de834aa18a77f0a85c4446378b96
Jeremy Cutler <cutler.jeremy@gmail.com>
2012-02-01 14:40:01 -0500

    add more to FF screnshotting

--- firefox/missinge/lib/main.js
+++ firefox/missinge/lib/main.js
@@ -1365,11 +1365,53 @@ function versionCompare(v1, v2) {
    }
 }
 
+function decode64(input) {
+   var output = "";
+   var chr1, chr2, chr3 = "";
+   var enc1, enc2, enc3, enc4 = "";
+   keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+   var i = 0;
+
+   // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+   var base64test = /[^A-Za-z0-9\+\/\=]/g;
+   if (base64test.exec(input)) {
+      debug("There were invalid base64 characters in the input text.");
+      return;
+   }
+   input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+   do {
+      enc1 = keyStr.indexOf(input.charAt(i++));
+      enc2 = keyStr.indexOf(input.charAt(i++));
+      enc3 = keyStr.indexOf(input.charAt(i++));
+      enc4 = keyStr.indexOf(input.charAt(i++));
+
+      chr1 = (enc1 << 2) | (enc2 >> 4);
+      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+      chr3 = ((enc3 & 3) << 6) | enc4;
+
+      output = output + String.fromCharCode(chr1);
+
+      if (enc3 != 64) {
+         output = output + String.fromCharCode(chr2);
+      }
+      if (enc4 != 64) {
+         output = output + String.fromCharCode(chr3);
+      }
+
+      chr1 = chr2 = chr3 = "";
+      enc1 = enc2 = enc3 = enc4 = "";
+
+   } while (i < input.length);
+
+   return unescape(output);
+}
+
 function takeScreenshot(x, y, w, h) {
-   var win = Cc['@mozilla.org/appshell/window-mediator;1']
-               .getService(Ci.nsIWindowMediator)
-               .getMostRecentWindow("navigator:browser").gBrowser.contentWindow;
-   window = Cc[
+   var window = Cc['@mozilla.org/appshell/window-mediator;1']
+                  .getService(Ci.nsIWindowMediator)
+                  .getMostRecentWindow("navigator:browser").gBrowser.contentWindow;
+
    x = x ? x : 0;
    y = y ? y : 0;
    w = w ? w : window.innerWidth - x;
@@ -1521,6 +1563,11 @@ function handleMessage(message, myWorker) {
                        tags: message.tags});
          myWorker.tab.url = message.url;
       }
+   }
+   else if (message.greeting == "screenshot") {
+      var dataURI = takeScreenshot(0,0,500,500);
+      dataURI = dataURI.replace(/^[^,]*,/);
+      var binData = decode64(dataURI);
    }
    else if (message.greeting == "getCrushes") {
       var i;
