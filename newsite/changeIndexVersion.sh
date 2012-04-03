674212a07757c8c55f03e14480e94ee73c4ddd49
Jeremy Cutler <cutler.jeremy@gmail.com>
2012-04-03 06:46:43 -0400

    more work on new site

--- newsite/changeIndexVersion.sh
+++ newsite/changeIndexVersion.sh
@@ -3,4 +3,7 @@ sed -e "s/<div version=\"[^\"]*\" id=\"versioncheck\"/<div version=\"$1\" id=\"v
 sed -e "s/<span id=\"download_version\">Version [0-9\.]*<\/span>/<span id=\"download_version\">Version $1<\/span>/" ~/webapps/missinge/index.new.html > ~/webapps/missinge/index.html
 rm ~/webapps/missinge/index.new.html
 
+sed -e "s/<div version=\"[^\"]*\" id=\"versioncheck\"/<div version=\"$1\" id=\"versioncheck\"/" ~/webapps/missinge/update.html > ~/webapps/missinge/update.new.html
+mv ~/webapps/missinge/update.new.html ~/webapps/missinge/update.html
+
 echo "var currentVersion=\"$1\";" > ~/webapps/missinge/currentVersion.js
