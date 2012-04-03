cadfcd2e424b9b8f37c12544d639c918079a7fca
Jeremy Cutler <cutler.jeremy@gmail.com>
2012-04-03 04:36:27 -0400

    Download page

---
+++ newsite/changeFirefoxURL.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "var firefoxURL=\"$1\";" > ~/webapps/missinge/firefoxURL.js
---
+++ newsite/firefoxURL.js
@@ -0,0 +1,1 @@
+var firefoxURL="https://addons.mozilla.org/firefox/downloads/file/148294/missing_e-2.8.1-fx.xpi";
