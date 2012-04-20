7f062d8e99b381be791d781f389d782050ae1bd1
Jeremy Cutler <cutler.jeremy@gmail.com>
2012-04-20 16:04:36 -0400

    new new submit bug page

--- site/submitbug.php
+++ site/submitbug.php
@@ -1,24 +1,19 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" dir="ltr">
-<!--
-
- Turnabout is fair play, sir.
-
--->
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="tumblr,missinge,missing,e,feature,features,blog,blogging" />
 <meta name="description" content="Missing e - The unofficial browser extension for Tumblr!"/>
 <meta name="author" content="Jeremy Cutler" />
 <meta name="robots" content="FOLLOW,INDEX" />
-<title>Submit Bug Report - Missing e - The browser extension for Tumblr!</title>
-<link href="http://fonts.googleapis.com/css?family=Bitter:400,700,400italic" rel="stylesheet" type="text/css">
+<title>Submit Bug Report - Missing e - The original browser extension for Tumblr!</title>
+<link href='http://fonts.googleapis.com/css?family=Kreon:300,400,700' rel='stylesheet' type='text/css'>
 <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
 <link rel="icon" href="/favicon.png" type="image/x-icon" />
 <script type="text/javascript" src="browser.js"></script>
 <script type="text/javascript" src="jquery-1.7.2.min.js"></script>
 <link rel="stylesheet" type="text/css" href="reset.css" />
-<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="sitestyles.css" />
 <?php
 
 $submission = isset($_REQUEST['bugsubmission']);
@@ -177,107 +172,115 @@ jQuery(document).ready(function($) {
 } //submission
 
 ?>
-</head>
 <body id="submitbug">
-<div id="header_container">
- <div id="header">
-  <div id="header_bar">
-   <a id="home_link" href="/"></a>
-   <div id="header_links">
-    <a href="features">Features</a>
-    <a href="faq">FAQ</a>
-    <a href="troubleshoot" class="selected">Troubleshoot</a>
-    <a href="https://www.paypal.com/ca/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=EGQCRBB2BH5U8">Donate</a>
+<header>
+ <nav>
+  <div id="nav_bar">
+   <a href="/" id="home_nav"><span id="home_navbg"></span><span class="subtext">HOME</span><img src="images/missinge100.png" alt="Home" /></a>
+   <div id="nav_links">
+    <a href="features" class="features_nav">FEATURES<span class="subtext">WHAT IT'S FOR.</span></a>
+    <a href="faq" class="faq_nav">FAQ<span class="subtext">QUESTIONS?</span></a>
+    <a href="troubleshoot" class="problems_nav selected_nav">PROBLEMS<span class="subtext">LET'S TROUBLESHOOT!</span></a>
+    <a href="http://blog.missing-e.com" class="blog_nav">BLOG<span class="subtext">STAY UPDATED.</span></a>
+    <a href="https://www.paypal.com/ca/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=EGQCRBB2BH5U8" class="donate_nav">DONATE<span class="subtext">SUPPORT MISSING E!</span></a>
    </div>
   </div>
- </div>
-</div>
-<div id="banner_container">
+ </nav>
  <div id="banner">
-  <div id="banner_box">
-   <h1>Report a bug.</h1>
-   <h2>Because spraying the code with bug repellent doesn't work!</h2>
+  <div id="icon_ribbon">
+   <div id="page_icon">
+    <img src="images/bugspray.png" alt="Submit Bug Report" style="margin-top:5px;opacity:0.95;" />
+   </div>
   </div>
-  <img id="banner_img" style="width:199px;height:181px;margin-top:-25px;margin-right:20px" src="images/bugspray.png" alt="Submit a Bug Report" />
+  <div id="icon_ribbon_rem"></div>
+  <div id="banner_text">
+   <hgroup>
+    <h1>Report a bug.</h1>
+    <h2>Because spraying the code with repellent doesn't work!</h2>
+   </hgroup>
+  </div>
  </div>
+</header>
+<div id="ad_title">Advertisement</div>
+<div id="ad">
+ <script type="text/javascript">
+  <!--
+   google_ad_client = "ca-pub-3604999147338055";
+   /* New Site */
+   google_ad_slot = "9843551983";
+   google_ad_width = 728;
+   google_ad_height = 90;
+  //-->
+ </script>
+ <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
 </div>
-<div id="ads">
- <div id="ads_title">Sponsored<br />Links</div>
-<script type="text/javascript"><!--
-google_ad_client = "ca-pub-3604999147338055";
-/* New Site */
-google_ad_slot = "9843551983";
-google_ad_width = 728;
-google_ad_height = 90;
-//-->
-</script>
-<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
-</div>
-<div class="content_container">
- <div class="content">
+<div id="main_content">
 <?php
 
 if ($submission) {
 
 ?>
-  <div class="troubleshoot_box">
-   <h1>Done!</h1>
-   <p>Thanks for submitting a bug report. You'll be contacted soon!</p>
-   <a href="/" class="button greenbutton">Home</a>
-  </div>
+ <div id="bugreport_done">
+  <h1>Done!</h1>
+  <p>Thanks for submitting a bug report. You'll be contacted soon!</p>
+  <a href="/" class="button greenbutton">Home</a>
+ </div>
 <?php
 
 }
 else {
 
 ?>
-  <form id="bugform" action="submitbug" method="post">
-   <input type="hidden" id="bugsubmission" name="bugsubmission" value="1" />
-   <input type="hidden" id="options" name="options" value="" />
-   <input type="hidden" id="timestamp" name="timestamp" value="" />
-   <input type="hidden" id="reason" name="reason" value="<?=$inputReason;?>" />
-   <input type="hidden" id="os" name="os" value="" />
-   <input type="hidden" id="browser" name="browser" value="" />
-   <input type="hidden" id="browserversion" name="browserversion" value="" />
-   <input type="hidden" id="missingeversion" name="missingeversion" value="" />
-   <div class="troubleshoot_box">
-    <h1>Submit Bug Report</h1>
-    <div class="troubleshoot_item">
-     <div class="step_icon unnumberedstep"></div>
-     <h2>Your Computer</h2>
-     <p><strong>Operating System:</strong> <span id="os_text"></span></p>
-     <p><strong>Browser:</strong> <span id="browser_text"></span></p>
-     <p><strong>Browser Version:</strong> <span id="browserversion_text"></span></p>
-     <p><strong>Missing e Version:</strong> <span id="missingeversion_text"></span></p>
-    </div>
-    <div class="troubleshoot_item">
-     <div class="step_icon unnumberedstep"></div>
-     <h2>The Problem You Are Having</h2>
-     <p><strong>Problem Type:</strong> <?=$inputReason;?></p>
-     <p style="margin-top:6px;">Please describe the problem:</p>
-     <textarea id="report" name="report" placeholder="What, exactly, is the problem? What were you doing when the problem came up? What steps are necessary to reproduce the problem?"></textarea>
-    </div>
-    <div class="troubleshoot_item">
-     <div class="step_icon unnumberedstep"></div>
-     <h2>Contact Email</h2>
-     <div><input type="text" name="thec" id="thec" style="width:200px" /></div>
-     <p style="font-size:12px;">Required. Your email address will not be used except for communicating with you for bug investigation.</p>
-    </div>
-    <div id="formbuttons"><button type="submit" title="Send" class="button greenbutton"><span>Send</span></button><a href="troubleshoot" title="Cancel" class="button greybutton">Cancel</a></div>
-   </div>
-  </form>
+ <form id="bugform" action="submitbug" method="post">
+  <input type="hidden" id="bugsubmission" name="bugsubmission" value="1" />
+  <input type="hidden" id="options" name="options" value="" />
+  <input type="hidden" id="timestamp" name="timestamp" value="" />
+  <input type="hidden" id="reason" name="reason" value="<?=$inputReason;?>" />
+  <input type="hidden" id="os" name="os" value="" />
+  <input type="hidden" id="browser" name="browser" value="" />
+  <input type="hidden" id="browserversion" name="browserversion" value="" />
+  <input type="hidden" id="missingeversion" name="missingeversion" value="" />
+  <h1>Submit Bug Report</h1>
+  <div class="bugreport_box wide_postit">
+   <hgroup>
+    <div class="step_icon unnumberedstep"></div>
+    <h2>Your Computer</h2>
+   </hgroup>
+   <p><strong>Operating System:</strong> <span id="os_text"></span></p>
+   <p><strong>Browser:</strong> <span id="browser_text"></span></p>
+   <p><strong>Browser Version:</strong> <span id="browserversion_text"></span></p>
+   <p><strong>Missing e Version:</strong> <span id="missingeversion_text"></span></p>
+  </div>
+  <div class="bugreport_box">
+   <hgroup>
+    <div class="step_icon unnumberedstep"></div>
+    <h2>The Problem You Are Having</h2>
+   </hgroup>
+    <p><strong>Problem Type:</strong> <?=$inputReason;?></p>
+    <p style="margin-top:15px" class="bugreport_msg">Please describe the problem:</p>
+    <textarea id="report" name="report" placeholder="What, exactly, is the problem? What were you doing when the problem came up? What steps are necessary to reproduce the problem?"></textarea>
+  </div>
+  <div class="bugreport_box">
+   <hgroup>
+    <div class="step_icon unnumberedstep"></div>
+    <h2>Contact Email</h2>
+   </hgroup>
+   <p class="bugreport_msg">Required. Your email address will not be used except for communicating with you for bug investigation.</p>
+   <div class="input_box"><input type="text" name="thec" id="thec" style="width:200px" placeholder="you@somewhere.com" /></div>
+  </div>
+  <div id="formbuttons"><button type="submit" title="Send" class="button greenbutton"><span>Send</span></button><a href="troubleshoot" title="Cancel" class="button silverbutton">Cancel</a></div>
+ </form>
 <?php
 
 }
 
 ?>
- </div>
 </div>
 <div id="footer_container">
  <div id="footer">
- &copy; 2012 Jeremy Cutler<br />
- <small>An open-source project released under <a class="link" href="http://www.gnu.org/licenses/gpl.html" title="GPL v3 License">GPL v3</a></small>
- <a class="greybutton button" style="float:right;" href="http://blog.missing-e.com" title="Missing e on Tumblr">Follow Missing e on Tumblr</a>
+  &copy; 2012, Jeremy Cutler<br />
+  <small>An open-source project released under <a class="link" href="http://www.gnu.org/licenses/gpl.html" title="GPL v3 License">GPL v3</a></small>
+  <a class="button silverbutton" href="http://blog.missing-e.com" title="Missing e on Tumblr">Follow Missing e on Tumblr</a>
  </div>
 </div>
 </body>
