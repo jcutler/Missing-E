2cc4a7a4fd8e0e8de995235bdd12ff3e694b6fe9
Jeremy Cutler <cutler.jeremy@gmail.com>
2012-04-21 11:30:53 -0400

    New post crushes layout

--- serverTools/crushes/index.php
+++ serverTools/crushes/index.php
@@ -22,8 +22,8 @@ if (isset($_GET['large'])) {
    $large = true;
 }
 else {
-   $avSize = 96;
-   $avCrop = 70;
+   $avSize = 64;
+   $avCrop = 64;
    $large = false;
 }
 
@@ -31,6 +31,9 @@ $curlHandle = curl_multi_init();
 
 for ($i=0; $i<9; $i++) {
    $prefix = "";
+   if (!isset($_GET["img" . $i])) {
+      die();
+   }
    if (preg_match('/^images\/default_avatar/',$_GET["img" . $i]) != 0) {
       $prefix = "http://www.tumblr.com/";
    }
@@ -38,7 +41,12 @@ for ($i=0; $i<9; $i++) {
       $prefix = "http://media.tumblr.com/";
    }
    $crushes[] = $prefix . preg_replace('/[0-9]*\.(png|jpg|gif|jpeg)$/i',"$avSize.$1",$_GET["img" . $i]);
-   $crushpercent[] = $_GET["per" . $i] . "%";
+   if (isset($_GET["per" . $i])) {
+      $crushpercent[] = $_GET["per" . $i] . "%";
+   }
+   else {
+      $crushpercent[] = "";
+   }
 }
 
 for ($i=0; $i<9; $i++) {
@@ -72,62 +80,52 @@ for ($i=0; $i<9; $i++) {
    imagedestroy($tmp);
 }
 
-$offset = $large ? 17 : 12;
-$img = imagecreatetruecolor($avCrop*3+$offset*2,$avCrop*3+$offset*2);
-$bl = imagecolorallocatealpha($img, 0, 0, 0, 60);
+$offset = 10;
+$img = imagecreatetruecolor($avCrop*3+$offset*4,$avCrop*3+$offset*4);
 $wh = imagecolorallocate($img, 255, 255, 255);
-imagefilledrectangle($img, 0, 0, $avCrop*3+$offset*2, $avCrop*3+$offset*2, $wh);
 
 if ($large) {
    $overlay['w'] = 44;
-   $overlay['h'] = 24;
+   $overlay['h'] = 22;
    $overlay['font'] = 5;
 }
 else {
    $overlay['w'] = 28;
-   $overlay['h'] = 16;
+   $overlay['h'] = 14;
    $overlay['font'] = 2;
 }
 
 for ($i=0; $i<9; $i++) {
-   $x = ($i%3)*$avCrop+$offset;
-   $y = ($i-$i%3)/3*$avCrop+$offset;
-   imagecopy($img, $cimg[$i], ($i%3)*$avCrop+$offset, ($i-$i%3)/3*$avCrop+$offset, 0, 0, $avCrop, $avCrop);
-   if ($i!=8)imagedestroy($cimg[$i]);
-   if(isset($_GET['showPercent'])) {
-      imagefilledrectangle($img, $x+$avCrop-$overlay['w'], $y+$avCrop-$overlay['h'], $x+$avCrop, $y+$avCrop, $bl);
+   $x = ($i%3)*($avCrop+$offset)+$offset;
+   $y = ($i-$i%3)/3*($avCrop+$offset)+$offset;
+   imagecopy($img, $cimg[$i], $x, $y, 0, 0, $avCrop, $avCrop);
+   imagedestroy($cimg[$i]);
+}
+
+if ($large) {
+   $froverlay = imagecreatefrompng('overlay_large.png');
+   $peroverlay = imagecreatefrompng('percentoverlay_large.png');
+}
+else {
+   $froverlay = imagecreatefrompng('overlay_small.png');
+   $peroverlay = imagecreatefrompng('percentoverlay_small.png');
+}
+imagecopy($img,$froverlay,0,0,0,0,$large?424:232,$large?424:232);
+if(isset($_GET['showPercent'])) {
+   imagecopy($img,$peroverlay,0,0,0,0,$large?424:232,$large?424:232);
+   for ($i=0; $i<9; $i++) {
+      $x = ($i%3)*($avCrop+$offset)+$offset;
+      $y = ($i-$i%3)/3*($avCrop+$offset)+$offset;
+      $xoff = $large ? 5 : 2;
+      $yoff = $large ? 6 : 3;
       $twidth = imagefontwidth($overlay['font'])*strlen($crushpercent[$i]);
-      $tx = $x+$avCrop-$overlay['w']/2-(ceil($twidth/2));
-      $ty = $y+$avCrop-$overlay['h']/2-(ceil(imagefontheight($overlay['font'])/2));
+      $tx = $x+$avCrop-$overlay['w']/2-(ceil($twidth/2))-$xoff;
+      $ty = $y+$avCrop-$overlay['h']/2-(ceil(imagefontheight($overlay['font'])/2))-$yoff;
       imagestring($img, $overlay['font'], $tx, $ty, $crushpercent[$i], $wh);
    }
 }
-
-$tmp=imagecreatetruecolor($avCrop*3+$offset*2,$avCrop*3+$offset*2);
-$twh = imagecolorallocate($tmp,255,255,255);
-$ttr = imagecolorallocate($tmp,0,0,0);
-$radius = $large?13:10;
-imagefilledrectangle($tmp, 0, 0, $avCrop*3+$offset*2, $avCrop*3+$offset*2, $twh);
-imagefilledrectangle($tmp, $offset+$radius, $offset, $offset+$avCrop*3-$radius-1, $offset+$avCrop*3-1, $ttr);
-imagefilledrectangle($tmp, $offset, $offset+$radius, $offset+$avCrop*3-1, $offset+$avCrop*3-$radius-1, $ttr);
-imagefilledellipse($tmp,$offset+$radius,$offset+$radius,$radius*2,$radius*2, $ttr);
-imagefilledellipse($tmp,$offset+$avCrop*3-$radius-1,$offset+$radius,$radius*2,$radius*2, $ttr);
-imagefilledellipse($tmp,$offset+$avCrop*3-$radius-1,$offset+$avCrop*3-$radius-1,$radius*2,$radius*2, $ttr);
-imagefilledellipse($tmp,$offset+$radius,$offset+$avCrop*3-$radius-1,$radius*2,$radius*2, $ttr);
-imagecolortransparent($tmp,$ttr);
-imagecopymerge($img, $tmp, 0, 0, 0, 0, $avCrop*3+$offset*2, $avCrop*3+$offset*2, 100);
-imagecolordeallocate($tmp, $twh);
-imagecolordeallocate($tmp, $ttr);
-imagedestroy($tmp);
-
-if ($large) {
-   $heart = imagecreatefrompng('heart_large.png');
-}
-else {
-   $heart = imagecreatefrompng('heart_small.png');
-}
-imagecopy($img,$heart,0,0,0,0,$large?59:40,$large?56:38);
-imagedestroy($heart);
+imagedestroy($froverlay);
+imagedestroy($peroverlay);
 
 /*
 header("Cache-Control: public");
@@ -137,8 +135,8 @@ header("Content-Disposition: attachment; filename=crushes.png");
 
 header('Content-type: image/jpeg');
 imagejpeg($img, NULL, 80);
+
 imagecolordeallocate($img, $wh);
-imagecolordeallocate($img, $bl);
 
 imagedestroy($img);
 
