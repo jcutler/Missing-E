973e26a05971c0e71d22690182a3d874fecfec59
Jeremy Cutler <cutler.jeremy@gmail.com>
2012-06-29 11:41:42 -0400

    Site update fixes for new PHP update page

--- site/changeFirefoxURL.sh
+++ site/changeFirefoxURL.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "var firefoxURL=\"$1\";" > ~/webapps/missinge/firefoxURL.js
 echo "<?php" > ~/webapps/missinge/firefoxURL.php
 echo "\$firefoxURL=\"$1\";" >> ~/webapps/missinge/firefoxURL.php
 echo "?>" >> ~/webapps/missinge/firefoxURL.php
